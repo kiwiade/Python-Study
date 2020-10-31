a21d96f697143dc67234414bf1ec7c48ddd2d3a0
kiwiade <bluestar373@naver.com>
2020-10-31 16:16:11 +0900

    add: project files

---
+++ variable_declaration.py
@@ -0,0 +1,20 @@
+num = 1
+name = 'Mike'
+is_ok = True
+
+print(num , type(num))
+print(name, type(name))
+print(is_ok, type(is_ok))
+
+# num = name
+# print(num, type(num))
+name = '2'
+new_int = int(name)
+print(new_int, type(new_int))
+new_str = str(num)
+print(new_str, type(new_str))
+
+num: int = 3
+name: str ='2'
+print(num, type(num))
+print(name, type(name))
