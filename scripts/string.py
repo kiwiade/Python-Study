e116ea05ca064b86d24c535ed540f96fcb69a6e0
kiwiade <bluestar373@naver.com>
2020-10-31 18:56:36 +0900

    add: using strings

---
+++ scripts/string.py
@@ -0,0 +1,20 @@
+print('hello')
+print("hello")
+print("I don't know")
+print('I don\'t know')
+print('say "I don\'t know"')
+print("say \"I don't know\"")
+print('hello.\nHow are you?')
+print(r'C:\name\name')
+print("""\
+line1
+line2
+line3
+line4\
+""")
+
+print('Hi.' * 3)
+print('Hi.' * 3 + 'Mike.')
+print('Py''thon')
+prefix = 'Py'
+print(prefix + 'thon')
