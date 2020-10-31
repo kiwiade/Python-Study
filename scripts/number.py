ef2736b2da4ac9e7c4564181b1df6c9608ad723d
kiwiade <bluestar373@naver.com>
2020-10-31 16:42:55 +0900

    add: using number

---
+++ scripts/number.py
@@ -0,0 +1,23 @@
+print(2+2)
+print(5*6)
+a: int = 8
+b: int = 5
+print(a/b)
+print(type(1))
+print(type(1.6))
+print(0.6)
+print(.6)
+print(17/3)
+print(17//3)
+print(17%3)
+print(5*5)
+print(5**2)
+print(5*5*5*5)
+print(5**4)
+print(round(3.141592, 2))
+
+import math
+result = math.sqrt(25)
+print(result)
+y = math.log2(10)
+print(y)
